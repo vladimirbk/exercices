ccf97c4dcc6e2585a7c820fff1deaf6dc4e0c8ae
vladimir.bk <befoulers@gmail.com>
2020-09-25 16:28:23 +0200

    WIP: chessboard

--- chapter2.js
+++ chapter2.js
@@ -5,11 +5,11 @@
 
 // FizzBuzz
 // for (let num = 1; num <= 100; num++) {
-//     if(num % 3 === 0 && num % 5 === 0){
+//     if(num % 3 === 0 && num % 5 === 0) {
 //         console.log('FizzBuzz');
-//     }else if(num % 5 === 0){
+//     }else if (num % 5 === 0) {
 //         console.log('Buzz');
-//     }else if(num % 3 === 0){
+//     }else if (num % 3 === 0) {
 //         console.log('Fizz');
 //     }else{
 //         console.log(num);
@@ -21,9 +21,9 @@ let size = 8;
 let j = '#';
 
 while (j.length <= Math.pow(size,2)) {
-    if(j.length % 8 === 0){
+    if (j.length % 9 === 0) {
         j += '\n';
-    } else if(j.length % 2 === 0) {
+    } else if (j.length % 2 === 0) {
         j += '#'
     } else {
         j += ' ';

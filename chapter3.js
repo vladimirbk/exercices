9e619286f06c0e3c8c049e2876e909ebb18c8a98
vladimir.bk <befoulers@gmail.com>
2020-10-04 20:56:31 +0200

    WIP: Recursion exercise added

--- chapter3.js
+++ chapter3.js
@@ -1,4 +1,5 @@
 //Minimum function
+
 const min = function(num1, num2) {
     if (typeof num1 !== 'number' || typeof num2 !== 'number') {
         return console.log('Enter numbers');
@@ -10,3 +11,18 @@ const min = function(num1, num2) {
         return console.log(num1);
     }
 };
+
+//Recursion
+
+function testEven(num) {
+    let isEven = true;
+    if (num === 0) {
+        isEven = true;
+        console.log(isEven);
+    } else if (num === 1) {
+        isEven = false;
+        console.log(isEven);
+    } else if (num > 1){
+        testEven(num - 2);
+    }
+}

f423fad3015b358dd690ce075bcebd2b557bf75b
vladimir.bk <befoulers@gmail.com>
2020-10-25 23:16:09 +0100

    Chapter 4: A list and Deep comparison exercises added

--- chapter4.js
+++ chapter4.js
@@ -52,3 +52,55 @@ function reverseArrayInPlace(array) {
     }
     return array;
   }
+
+//A List
+function arrayToList(array) {
+    let list = null;
+    for(let i = array.length - 1; i >= 0; i--) {
+        list = { value: array[i], rest: list }
+    }
+    return list;
+}
+
+function listToArray(list) {
+    let array = [];
+    for(let node = list; node;  node = node.rest){
+        array.push(node.value);
+    }
+    return array;
+}
+
+function prepend(value, list) {
+    return { value, rest: list };
+}
+
+function nth(list, num) {
+    if (!list) {
+        return undefined;
+    } else if (num == 0) {
+        return list.value
+    } else {
+        return nth (list.rest, num - 1)
+    }
+}
+
+//Deep comparison
+function deepEqual(a, b) {
+    if (a === b) {
+        return true;
+    }
+    if (a == null || typeof a != "object" || b == null || typeof b != "object") { 
+        return false;
+    } 
+  
+    let keysA = Object.keys(a), keysB = Object.keys(b);
+    if (keysA.length != keysB.length) { 
+        return false; 
+    }
+    for (let key of keysA) {
+      if (!keysB.includes(key) || !deepEqual(a[key], b[key])) {
+          return false;
+      }
+    }
+    return true;
+  }
